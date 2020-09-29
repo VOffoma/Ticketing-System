dc8e0564f716ed54494e33af74962d9041c87093
Victoria Offoma <offoma.victoria@gmail.com>
2020-09-29 08:31:14 +0100

    chore: provide config that combines all env files.

---
+++ src/config/index.ts
@@ -0,0 +1,30 @@
+import dotenvExtended from 'dotenv-extended';
+import dotenvParseVariables from 'dotenv-parse-variables';
+
+const env = dotenvExtended.load({
+	path: process.env.ENV_FILE,
+	defaults: './config/.env.defaults',
+	schema: './config/.env.schema',
+	includeProcessEnv: true,
+	silent: false,
+	errorOnMissing: true,
+	errorOnExtra: true
+});
+
+const parsedEnv = dotenvParseVariables(env);
+
+interface Config {
+	port: number;
+	mongodb: {
+		url: string;
+	};
+}
+
+const config: Config = {
+	port: parsedEnv.PORT as number,
+	mongodb: {
+		url: parsedEnv.MONGODB_URL as string
+	}
+};
+
+export default config;
