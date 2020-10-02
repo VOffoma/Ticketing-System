52d35b55781315e18cf93019f16249d7bc0f5fe8
Victoria Offoma <offoma.victoria@gmail.com>
2020-10-02 20:06:44 +0100

    chore: use config file

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import 'dotenv/config';
 import app from './api/app';
 import logger from './utils/logger';
 import db from './utils/db';
+import config from './config';
 
 function startServer() {
 	db.open().then(() => {
-		const port = process.env.PORT || 7077;
+		const port = config.port || 7077;
 		app.set('port', port);
 
 		app.listen(port, () => {
